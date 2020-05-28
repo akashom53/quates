7aa6519399b7f7bf887bf1b5d4dca9f0f5da179c
akashom53 <akashom53@gmail.com>
2020-05-29 00:14:50 +0530

    Build generated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC25IDi68NLtfVDNaPaMImqjowX--1n-ao",
+    authDomain: "databases-6415d.firebaseapp.com",
+    databaseURL: "https://databases-6415d.firebaseio.com",
+    projectId: "databases-6415d",
+    storageBucket: "databases-6415d.appspot.com",
+    messagingSenderId: "1051312391323",
+    appId: "1:1051312391323:web:a1d2e82d5992196116f23e"
+  }
 };
