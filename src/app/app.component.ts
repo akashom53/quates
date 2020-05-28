6023d3e484fed27f9637d3f87164a58cb68f5110
akashom53 <akashom53@gmail.com>
2020-05-29 00:09:13 +0530

    Quote complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes';
+  title = 'Bible Quotes';
+
+  chapter = '';
+  verse = '';
+  quote = '';
+  quotes: Observable<any[]>;
+
+  constructor(public db: AngularFireDatabase) {
+    this.quotes = db.list('quotes').valueChanges();
+  }
+
+  onSubmit() {
+    this.db.list('quotes').push({
+      chapter: this.chapter,
+      verse: this.verse,
+      quote: this.quote,
+    });
+    this.chapter = '';
+    this.verse = '';
+    this.quote = '';
+  }
 }
